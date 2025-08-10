e5ef371b9aa8fe13523946a1f457eb087ff56ed5
Siddique Ahmed <Itssiddiue786@gmail.com>
2025-08-10 19:54:39 +0500

    otp align

--- src/pages/OTPCode.tsx
+++ src/pages/OTPCode.tsx
@@ -4,8 +4,8 @@ import OTPForm from "../components/otp_code/OTPForm";
 const OTPCode = () => {
   return (
     <div className="min-h-screen flex items-center bg-gray-100">
-      <ChatVerseDetail className="hidden sm:w-full min-h-screen md:flex items-center justify-center p-4" />
       <OTPForm />
+      <ChatVerseDetail className="hidden sm:w-full min-h-screen md:flex items-center justify-center p-4" />
     </div>
   );
 };
